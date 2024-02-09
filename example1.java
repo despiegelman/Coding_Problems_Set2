dcac5e9718fefebd84512133236b3ff880d0d3f2
dspiegelman <mtvesuvius67@gmail.com>
2024-02-09 14:41:41 -0500

    Finished these

---
+++ example1.java
@@ -0,0 +1,7 @@
+public class example1 {
+    public static void main (String[] args) {
+        System.out.println("\t\t Dog Show Winners");
+        System.out.println("\nPlace:\t1\t2\t3\t4\t5\t6");
+        System.out.println("Dog#: \t76\t32\t18\t47\t11\t29");
+    }
+}
